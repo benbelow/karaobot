7c073fda8bcbd0e53b3811e7bb809258512f9895
Ben Below <benjamin.below@softwire.com>
2022-08-31 13:19:42 +0100

    initiil xml pirsing ti siftwirify lyrics

--- mitmproxy-addons/karafun.py
+++ mitmproxy-addons/karafun.py
@@ -2,6 +2,7 @@
 Run as follows: mitmproxy -s karafun.py
 """
 from mitmproxy import ctx
+import xml.etree.ElementTree as ET
 
 class Karafun:
     # Intercept all responses
@@ -10,8 +11,33 @@ class Karafun:
         request_path = flow.request.path
 
         # Only modify the lyrics
-        if request_path.find("request.php"):
-            flow.response.text = flow.response.text.replace("creep", "crêpe")
+        if "request.php" in request_path:
+
+            text = flow.response.text
+            root = ET.fromstring(text)
+
+            karaoke = [x for x in root if x.tag == "karaoke"][0]
+            pages = [x for x in karaoke if x.tag == "page"]
+
+            for page in pages:
+                lines = [x for x in page if x.tag == "line"]
+                for line in lines:
+                    words = [x for x in line if x.tag == "word"]
+                    for word in words:
+                        syllables = [x for x in word if x.tag == "syllabe"]
+                        for syllable in syllables:
+                            text = [x for x in syllable if x.tag == "text"][0]
+                            text.text = text.text.replace("a", "i").replace("e", "i").replace("o", "i").replace("u", "i")
+
+            print("DONE")
+
+            final = ET.tostring(root, "unicode")
+
+            f = open("output.txt", "a")
+            f.write(final)
+            f.close()
+
+            flow.response.text = final
             # TODO: interpret response XML, replace words and reconstruct
 
 addons = [Karafun()]
